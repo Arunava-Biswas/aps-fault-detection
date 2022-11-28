7fce5f9468e718726ea05ef29cb2a1afeca72b42
Arunava-Biswas <arunavabiswas44@gmail.com>
2022-11-28 17:14:20 +0530

    updated

---
+++ setup.py
@@ -0,0 +1,31 @@
+# importing libraries
+from setuptools import find_packages, setup
+from typing import List
+
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+HYPHEN_E_DOT = "-e ."
+
+# Creating function to make list of library names required for this project
+def get_requirements()->List[str]:
+    
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+    
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_list
+
+
+# Creating setup for the library where we can state the version of the library
+# The find_packages() is used so it can find all the python source code from the 'sensor' folder
+# install_requires is for the list of other libraries we require for our project. 
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="ArunavaBiswas",
+    author_email="arunavabiswas44@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
