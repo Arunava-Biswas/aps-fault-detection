88625d0c8021707773adedee5360f021cf93afa2
Arunava-Biswas <arunavabiswas44@gmail.com>
2022-11-28 17:38:58 +0530

    updated requirements.txt file and steps

--- setup.py
+++ setup.py
@@ -7,12 +7,14 @@ REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
 # Creating function to make list of library names required for this project
+# the '-> List[str]' just tells that f() returns a list of strings
 def get_requirements()->List[str]:
     
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
+    # to remove the '-e .' when installing the requirements.txt
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
